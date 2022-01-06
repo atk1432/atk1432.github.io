4e383764ab1856a4b81878870eb8c9a0d876e638
atk1432 <hiepnguyenvietatk@gmail.com>
2022-01-06 22:29:25 +0700

    add pause func

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,24 +1,33 @@
 var pauseButton = document.querySelector('.music-player__footer-pause')
 var music = document.querySelector('audio')
 
-var musicPlaying = false
-
-pauseButton.onclick = () => {
-    if (!musicPlaying) {
-        music.play()
-        musicPlaying = true
-        pauseButton.classList.replace('fa-play', 'fa-pause')
-    } else {
-        music.pause()
-        musicPlaying = false
-        pauseButton.classList[1] = 'fa-play'
-        pauseButton.classList.replace('fa-pause', 'fa-play')
+document.onkeydown = (e) => {
+    if (e.code == 'Space') {
+        e.preventDefault()
+        playMusic()
     }
 }
 
-function rotateCD() {
-    var cd = document.querySelector('.music-player__body-cd')
+pauseButton.onclick = () => {
+    playMusic()
+}
 
+music.onended = () => {
+    music.pause()
+    pauseButton.classList.replace('fa-pause', 'fa-play')
+}
+
+
+function playMusic() {
     
+
+    if (music.paused) {
+        music.play()
+        pauseButton.classList.replace('fa-play', 'fa-pause')
+    } else {
+        music.pause()
+        pauseButton.classList.replace('fa-pause', 'fa-play')
+    }
+
 }
 
